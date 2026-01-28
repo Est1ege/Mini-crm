2235eefcb36179cef36bb05f8c11ab6d633a4b95
Est1ege <124670997+Est1ege@users.noreply.github.com>
2026-01-28 22:42:53 +0500

    add rate limiting

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\StatisticsController;
 use App\Http\Controllers\Api\TicketController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/tickets', [TicketController::class, 'store']);
+Route::post('/tickets', [TicketController::class, 'store'])
+    ->middleware('throttle:tickets');
 Route::get('/tickets/statistics', [StatisticsController::class, 'index'])
     ->middleware('auth:sanctum');
