4002409eda177e32cb40495adda207a6270eccdb
Est1ege <124670997+Est1ege@users.noreply.github.com>
2026-01-28 22:42:15 +0500

    add auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\TicketController as AdminTicketController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WidgetController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,9 +11,21 @@ Route::get('/', function () {
 
 Route::get('/widget', [WidgetController::class, 'index'])->name('widget');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::middleware(['auth', 'manager'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/tickets', [AdminTicketController::class, 'index'])->name('tickets.index');
     Route::get('/tickets/{id}', [AdminTicketController::class, 'show'])->name('tickets.show');
     Route::patch('/tickets/{id}/status', [AdminTicketController::class, 'updateStatus'])->name('tickets.update-status');
     Route::get('/tickets/{ticketId}/download/{mediaId}', [AdminTicketController::class, 'download'])->name('tickets.download');
 });
+
+require __DIR__.'/auth.php';
